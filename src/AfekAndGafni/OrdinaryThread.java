c64ae6e15e7bb1dcf2cf0171ad6c082bc3093825
Manuel Ribeiro <manuelemosr@gmail.com>
2015-05-14 17:24:47 +0200

    Added comparisions between (level,id) messages | The functions are not working yet

--- src/AfekAndGafni/OrdinaryThread.java
+++ src/AfekAndGafni/OrdinaryThread.java
@@ -1,26 +1,54 @@
 package AfekAndGafni;
 
-public class OrdinaryThread implements Runnable {
+import java.util.ArrayList;
 
+public class OrdinaryThread implements Runnable {
+	ArrayList<Integer> LevelList;
+	ArrayList<Integer> IdList;
+	Integer OrdinaryId = Init.Process_Number;
+	Integer OrdinaryLevel = 0;
 	
 	public OrdinaryThread(ProcessID aux_pid) {
 		// TODO Auto-generated constructor stub
 	}
 
 	public void run() {
-		// TODO Auto-generated method stub
+		Integer LevelAux;
+		Integer IdAux;
 		
-		/*
-		 * Wait for Candidate Message
-		 * 
-		 * Implement algorithm
-		 * 
-		 * 
-		 * 
-		 * 
-		 *
-		 */
+		while(!Init.ConnectionsReady){
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(!this.LevelList.isEmpty() && !this.IdList.isEmpty()){
+			/*Process Messages and remove them from the list*/
+			
+			LevelAux = this.RemoveElementLevel();
+			IdAux = this.RemoveElementId();
+			
+			if( LevelAux < OrdinaryLevel || ( LevelAux == OrdinaryLevel && IdAux < OrdinaryId ) ){
+				/* (level', id') < (level, id) */
+				/* Ignore */
+			}else if( LevelAux > OrdinaryLevel || ( LevelAux == OrdinaryLevel && IdAux > OrdinaryId ) ){
+				/* (level', id') < (level, id) */
+			}else if( LevelAux == OrdinaryLevel && IdAux == OrdinaryId ) ){
+				/* (level', id') = (level, id) */
+			}
+			
+		}
 
 	}
-
+	
+	public synchronized Integer RemoveElementLevel(){
+		return LevelList.remove(0);
+	}
+	public synchronized Integer RemoveElementId(){
+		return IdList.remove(0);
+	}
 }
+
+
